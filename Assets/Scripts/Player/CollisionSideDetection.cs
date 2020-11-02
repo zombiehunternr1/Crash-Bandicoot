37e9549fe88f54ad41f5eb1bee6eaffea37b9220
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-11-02 20:48:33 +0100

    Added collision-side detection
    Once the player hits a certain side of the cube it gets displayed in the debug logging. Need to adjust that this only happens when the player hits an crate object or an enemy object

---
+++ Assets/Scripts/Player/CollisionSideDetection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSideDetection : MonoBehaviour
+{
+    //Checks if the player collides with any gameobject
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(ReturnDirection(collision.gameObject, this.gameObject));
+    }
+
+    //Enums to help check which side the player hit a certain object.
+    private enum HitDirection { None, Top, Bottom, Forward, Back, Left, Right }
+
+    //This Enum function checks which side the player hits a certain object and returns this information.
+    private HitDirection ReturnDirection(GameObject Object, GameObject ObjectHit)
+    {
+        HitDirection hitDirection = HitDirection.None;
+        RaycastHit MyRayHit;
+        Vector3 direction = (Object.transform.position - ObjectHit.transform.position).normalized;
+        Ray MyRay = new Ray(ObjectHit.transform.position, direction);
+
+        if (Physics.Raycast(MyRay, out MyRayHit))
+        {
+            if (MyRayHit.collider != null)
+            {
+                Vector3 MyNormal = MyRayHit.normal;
+                MyNormal = MyRayHit.transform.TransformDirection(MyNormal);
+
+                if (MyNormal == MyRayHit.transform.up)
+                {
+                    hitDirection = HitDirection.Top;
+                }
+                if (MyNormal == -MyRayHit.transform.up)
+                {
+                    hitDirection = HitDirection.Bottom;
+                }
+                if (MyNormal == MyRayHit.transform.forward)
+                {
+                    hitDirection = HitDirection.Forward;
+                }
+                if (MyNormal == -MyRayHit.transform.forward)
+                {
+                    hitDirection = HitDirection.Back;
+                }
+                if (MyNormal == MyRayHit.transform.right)
+                {
+                    hitDirection = HitDirection.Right;
+                }
+                if (MyNormal == -MyRayHit.transform.right)
+                {
+                    hitDirection = HitDirection.Left;
+                }
+            }
+        }
+        return hitDirection;
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/Scripts/PlayerInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName="Playerinfo", menuName ="SO/Player")]
+public class PlayerInfo : ScriptableObject
+{
+    public int Lives;
+    public int ExtraHit;
+}
