5a84e0d651390d6ca7f3052f48bce01c9217e7ce
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-11-02 19:08:02 +0100

    Improved Player controls
    - Added in a player jump feature.
    - Camera now follows the player to it's position.
    - Added in a spin attack

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public float SmoothFactor;
+
+    private Vector3 CameraOffset;
+
+    //Gets the CameraOffset from it's own position minus the position of the player and stores it in the variable CameraOffset.
+    void Start()
+    {
+        CameraOffset = transform.position - PlayerTransform.transform.position;
+    }
+
+    //Repositions the camera to the players current position with the offset and make the transations look smooth.
+    void LateUpdate()
+    {
+        Vector3 NewPosition = PlayerTransform.transform.position + CameraOffset;
+        transform.position = Vector3.Slerp(transform.position, NewPosition, SmoothFactor);
+        transform.LookAt(PlayerTransform);
+    }
+}
