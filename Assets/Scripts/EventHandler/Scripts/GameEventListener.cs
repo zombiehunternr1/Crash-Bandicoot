577443b6549f22380f14c3e3f2d6c5e32c9d06d7
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-11-07 00:13:30 +0100

    Added GameEvents
    Added in a GameEvents script to handle certain game events that can happen when the player interacts with anything

---
+++ Assets/Scripts/EventHandler/Scripts/GameEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField] private GameEvent _gameEvent;
+    [SerializeField] private UnityEvent _response;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        _gameEvent.RegisterListener(this);
+    }
+
+    // Update is called once per frame
+    void OnDisable()
+    {
+        _gameEvent.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        _response.Invoke();
+    }
+}
