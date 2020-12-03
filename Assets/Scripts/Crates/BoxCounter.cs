f71355bb7a8efea5671d5c8792de18a0d71eb16a
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-12-03 22:10:27 +0100

    Added box counter
    - Added in a box counter that gets all the breakable crates in the level and adds it to it's own list.
    
    TO DO:
    - Update amount of crates broken by player display once player breaks a crate.
    - Once player has broken all the crates it should instanciate a gem and destroy itself afterwards.

---
+++ Assets/Scripts/Crates/BoxCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxCounter : MonoBehaviour
+{
+    public Text BoxCount;
+    public GameObject Boxes;
+
+    private List<Breakable> TotalCrates = new List<Breakable>();
+    private Renderer Rend;
+
+    //Gets all the breakable crates and stores them in the variable CratesInLevel.
+    //Then goes over each crate and adds that crate to the TotalCrates list.
+    //Once that is done it displays how many crates the player has currently broken and how many there are in the level.
+    void Start()
+    {
+        var CratesInLevel = Boxes.GetComponentsInChildren<Breakable>();
+
+        foreach(Breakable crate in CratesInLevel)
+        {
+            TotalCrates.Add(crate);
+        }
+        BoxCount.text = "0 / " + TotalCrates.Count.ToString();
+    }
+
+    //Keeps setting the BoxCount text to the objects position.
+    void FixedUpdate()
+    {
+        Vector3 Pos = Camera.main.WorldToScreenPoint(this.transform.position);
+        BoxCount.transform.position = Pos;        
+    }
+}
