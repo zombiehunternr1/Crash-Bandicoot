852f25b8096d47c9269e3fb196b331aca4c79209
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-12-06 17:28:38 +0100

    Added checkpoint animation
    - Added in a metal crate and breakable checkpoint crate prefab that will spawn in as soon as the player hit either an interactable or breakable checkpoint.
    - Added animation to the breakable checkpoint crate that as soon as the player interacts with it will create an animation that shows the crate falling apart.

--- Assets/Scripts/Crates/CrateType/CheckPoint.cs
+++ Assets/Scripts/Crates/CrateType/CheckPoint.cs
@@ -9,6 +9,9 @@ public class CheckPoint : MonoBehaviour
     [HideInInspector]
     public bool hasSet = false;
 
+    public GameObject MetalCrate;
+    public GameObject BrokenCheckpoint;
+
     private Transform CheckPointPosition;
 
     private void Awake()
@@ -26,10 +29,12 @@ public class CheckPoint : MonoBehaviour
             if (GetComponent<Breakable>())
             {
                 DestroyedCrate.Raise();
+                Instantiate(BrokenCheckpoint, transform.position + (Vector3.up * .1f), transform.rotation);
                 gameObject.SetActive(false);
             }
             else if (GetComponent<Interactable>())
             {
+                Instantiate(MetalCrate, transform.position, transform.rotation);
                 gameObject.SetActive(false);
             }
         }
