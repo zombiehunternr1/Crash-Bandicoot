7c2efdbaf8a686d92d81118186b10c23f332693f
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-12-10 00:07:16 +0100

    Added exit level with effect
    - Added in a particle effect that shows where the exit of the level is. It starts playing when the player enters the collision area and stops when leaving it.
    - Added in a script that loads the Testscene when the player enters the exit trigger area.
    
    NOTE:
    The load scene to Testscene needs to change to the next level.

---
+++ Assets/Scripts/Effects/Exit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public ParticleSystem ExitEffect;
+
+    private ParticleSystem Effect;
+
+    private void Awake()
+    {
+        Effect = Instantiate(ExitEffect, transform.position, ExitEffect.transform.rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerActions>())
+        {
+            Effect.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerActions>())
+        {
+            Effect.Stop();
+        }
+    }
+}
