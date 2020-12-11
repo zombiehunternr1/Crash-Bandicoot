ce2832792836a41dcc41a06ace453bd297472c59
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-12-11 01:52:55 +0100

    Improved exclamation mark animation
    Looked back at the orginal Crash Bandicoot and saw that they also had an exclamation mark on top of the crate and this one lasted longer the other once on the rest of the sides. I've added this in as well and the improvement looks a lot better and smoother now.

--- Assets/Scripts/Effects/ExclamationMark.cs
+++ Assets/Scripts/Effects/ExclamationMark.cs
@@ -7,6 +7,6 @@ public class ExclamationMark : MonoBehaviour
     //Destroys the gameobject after 1 second.
     private void Awake()
     {
-        Destroy(gameObject, 1f);
+        Destroy(gameObject, 3f);
     }
 }
