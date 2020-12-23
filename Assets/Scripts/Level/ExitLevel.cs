b79174ea024fe84dd11d2b9ac172a2f6865cdfa6
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-12-23 22:32:02 +0100

    Added fadeout exit
    - Added in a fadout effect once the player hits the exit effect. Afterwards it reloads the scene.
    - Saving still works because we don't immidiately load the scene while we are saving. We only load in the scene once the screen is comepletely black.

--- Assets/Scripts/Level/ExitLevel.cs
+++ Assets/Scripts/Level/ExitLevel.cs
@@ -2,18 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ExitLevel : MonoBehaviour
 {
     public GemCollected Gems;
+    public RectTransform FadePanel;
+
+    private int FadingSpeed = 1;
+    private bool Fading = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerActions>())
         {
-            //LevelData.Instance.Data.GemsCollected = Gems.GemsCollected.ToArray();
+            other.GetComponent<PlayerActions>().CanMove = false;
             SaveSystem.SaveProgress(other.GetComponent<PlayerStatus>().Player, LevelData.Instance.Data);
-            //SceneManager.LoadScene("Testscene");
+            StartCoroutine(FadeToBlack());
+        }
+    }
+
+    //This coroutine switches the panel from opaque to black.
+    IEnumerator FadeToBlack()
+    {
+        //Gets the color component from the panel and stores it in the FadingColor variable.
+        Color FadingColor = FadePanel.GetComponent<Image>().color;
+        float FadeAmount;
+        if (Fading)
+        {
+            //Keeps looping until the alpha color of the image isn't smaller then 1.
+            while (FadePanel.GetComponent<Image>().color.a < 1)
+            {
+                FadeAmount = FadingColor.a + (FadingSpeed * Time.deltaTime);
+                FadingColor = new Color(FadingColor.r, FadingColor.g, FadingColor.g, FadeAmount);
+                FadePanel.GetComponent<Image>().color = FadingColor;
+                yield return null;
+            }
+            yield return new WaitForSeconds(FadingSpeed);
+            Fading = false;
+            StartCoroutine(FadeToBlack());
+        }
+        else
+        {
+            SceneManager.LoadScene("TestScene");
         }
     }
 }
