60a9ca6775dd131d68d091087920f15dbd599e6a
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2020-12-25 20:29:00 +0100

    Added PlayerDamage script
    - Replaced ResetAfterDeath script with DamagePlayer script since that is what this function inside this script is going to do.
    - Build in a check to see if the object this script is attached to can instakill the player or not. If it can it doesn't matter if the player has invinsibility active or not, the player will die automatically.
    - If not it checks if the player has invinsibility active, if so it just kills the enemy type.
    - If the player has no invinsibility when he collides with the enemy it checks if the player as an extra hit left, if so it removes it from the player. If not then it kills the player.
    
    TO DO:
    - Build in invinsibility mode on the player that gets activated for a certain amount of time when the player has collected three extra hits. Once the player collides with an instakill object the invinsibility needs to stop at that point.
    - Build in a function that kills of the enemy once the player collides with it and the player is either in invinsibility mode or had an extra hit left. The enemy then needs to projectile away for a certain amount of time in the opposite direction the player collided with and after that time it gets destroyed. If the projectile enemy collides with either an crate or enemy the crate gets destroyed or the enemy gets destroyed.

---
+++ Assets/Scripts/Player/DamagePlayer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    public GameEvent PlayerHit;
+
+    public bool Instakill;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerActions>())
+        {
+            if (Instakill)
+            {
+                //Add remove invincibility functionality.
+                other.GetComponent<PlayerActions>().CanMove = false;
+                other.GetComponent<PlayerStatus>().Player.ExtraHit = 0;
+                PlayerHit.Raise();
+            }
+            else
+            {
+                if(other.GetComponent<PlayerStatus>().Player.ExtraHit != 3)
+                {
+                    if (other.GetComponent<PlayerStatus>().Player.ExtraHit <= 0)
+                    {
+                        other.GetComponent<PlayerActions>().CanMove = false;
+                        PlayerHit.Raise();
+                    }
+                    else
+                    {
+                        other.GetComponent<PlayerStatus>().Player.ExtraHit--;
+                    }
+                }
+                else
+                {
+                    //Kill enemy.
+                }
+            }         
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerActions>())
+        {
+            if (Instakill)
+            {
+                //Add remove invincibility functionality.
+                collision.gameObject.GetComponent<PlayerActions>().CanMove = false;
+                collision.gameObject.GetComponent<PlayerStatus>().Player.ExtraHit = 0;
+                PlayerHit.Raise();
+            }
+            else
+            {
+                if (collision.gameObject.GetComponent<PlayerStatus>().Player.ExtraHit != 3)
+                {
+                    if (collision.gameObject.GetComponent<PlayerStatus>().Player.ExtraHit <= 0)
+                    {
+                        collision.gameObject.GetComponent<PlayerActions>().CanMove = false;
+                        PlayerHit.Raise();
+                    }
+                    else
+                    {
+                        collision.gameObject.GetComponent<PlayerStatus>().Player.ExtraHit--;
+                    }
+                }
+                else
+                {
+                    //Kill enemy.
+                }
+            }
+        }
+    }
+}
